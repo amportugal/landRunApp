f29a93ba564d10c680bf95248e422347cc277c35
students@deti <studentsdeti@detis-mbp.lan>
2015-12-22 18:16:42 +0000

    Initial Commit

---
+++ LandRun/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LandRun
+//
+//  Created by students@deti on 22/12/15.
+//  Copyright © 2015 students@deti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
